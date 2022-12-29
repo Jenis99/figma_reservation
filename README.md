653784fc3673450cca277af2adc81f414d8def56
krutikagabani <jeniskapadiya99@gmail.com>
2022-12-29 12:27:13 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# figma_reservation
+ Figma To HTML
---
+++ js/main.js
@@ -0,0 +1,6 @@
+
+$(document).ready(function(){
+  $("#upclick").click(function(){
+    $("html, body").animate({scrollTop: 0}, 1000);
+  });
+});
